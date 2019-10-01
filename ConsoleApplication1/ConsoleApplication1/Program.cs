d32ad58d4aa2f5b913999008850e433d40f7cf59
yolochocopie <chocopieyolo@gmail.com>
2019-10-01 15:19:47 +0700

    01-10-2019

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            InputInt(array);
+        }
+
+        public static void InputInt(int[] Arr)
+        {
+            for (int i = 0; i <Arr.Length; i ++)
+            {
+                Console.Write(" A[{0}] =", i);
+                string str = Console.ReadLine();
+                Arr[i] = int.Parse(str);
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/number5/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace number5
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
